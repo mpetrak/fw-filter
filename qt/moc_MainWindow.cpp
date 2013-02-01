c94e93af86e5e62dcb8fa4d2218244a78750f0f5
mpetrak <petrak.mrtn@gmail.com>
2013-02-01 16:01:40 +0100

    Add nbproject folder to git.

--- qt/moc_MainWindow.cpp
+++ qt/moc_MainWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MainWindow.h'
 **
-** Created: Fri Jan 25 17:30:57 2013
+** Created: Fri Feb 1 16:01:04 2013
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- qt/moc_RuleEditWidget.cpp
+++ qt/moc_RuleEditWidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'RuleEditWidget.h'
 **
-** Created: Fri Jan 25 17:30:59 2013
+** Created: Fri Feb 1 16:01:06 2013
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
