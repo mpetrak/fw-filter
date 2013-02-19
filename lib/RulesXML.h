76ee0227412749c8cdd82ddf0d86763a4b60133b
mpetrak <petrak.mrtn@gmail.com>
2013-02-19 17:14:57 +0100

    Comments for saving rules to XML file

--- lib/RulesXML.h
+++ lib/RulesXML.h
@@ -35,15 +35,49 @@ public:
     
     RulesXML();
     
+    /**
+     * Save rules in list to XML file.
+     * @param rules list of rules
+     * @return true on success / false on fault
+     */
     bool saveRules(QList<FilterRule> rules);
+    
+    /**
+     * Load rules from XML file.
+     * @return list of loaded rules
+     */
     QList<FilterRule> loadRules();
     
 private:
+    /**
+     * Convert one rule to XML node.
+     * @param rule rule
+     * @return XML node with rule data
+     */
     xmlNodePtr rule2XmlNode(FilterRule *rule);
+    
+    /**
+     * Convert rule value to XML node.
+     * @param parent parent node of created node
+     * @param nodeName name of created node
+     * @param value created node value
+     * @param negation if value is negatiated
+     */
     void value2Node(xmlNodePtr *parent, const char *nodeName, char *value, bool negation);
+    
+    /**
+     * Convert rule address value to XML node
+     * @param parent parent node of created node
+     * @param nodeName name of created node
+     * @param address address value
+     * @param mask address mask value
+     * @param negation if address value is negatiated
+     */
     void address2Node(xmlNodePtr *parent, const char *nodeName, char *address, char *mask, bool negation);
 
+    /** XML document */
     xmlDocPtr doc;
+    /** XML document root node */
     xmlNodePtr rootNode;
 };
 
