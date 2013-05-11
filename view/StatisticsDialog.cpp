529fe713ed5fb0f30ca92f692a47554a96adaa1c
mpetrak <petrak.mrtn@gmail.com>
2013-05-11 18:18:44 +0200

    Make stats dialog more wider, if scrollbar visible, the ok button is covered by scrollbar

--- view/StatisticsDialog.cpp
+++ view/StatisticsDialog.cpp
@@ -21,8 +21,8 @@ StatisticsDialog::StatisticsDialog(QWidget *parent, QList<FilterRule> rules) : Q
         this->setObjectName(QString::fromUtf8("StatisticsDialog"));
     }
     this->setWindowTitle(QString::fromUtf8("Statistics"));
-    this->setFixedWidth(400);
-    this->resize(400, 300);
+    this->setFixedWidth(450);
+    this->resize(450, 300);
 
     /* central widget and layout */
     centralWidget = new QWidget(this);
